52028a59419f32e6cac269fe7cb0d041c5d5cb42
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-07-14 00:42:56 +0300

    modified: main.kt

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+package ru.netology
+
+fun main() {
+    val previousPurchaseAmount = 3_000
+    val purchaseAmount = 5_000
+    val discountStart = 10_000
+    val discountMedium = 1_000_000
+
+    val priceDiscountStart = purchaseAmount * discountStart
+    val priceDiscountMedium = purchaseAmount * discountMedium
+    if ( discountMedium > priceDiscountStart >  || < priceDiscountMedium)
+}
