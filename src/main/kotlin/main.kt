46cffb20c68dec800919b27ed73028d6872d505a
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-07-14 12:57:48 +0300

    Added loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,29 @@
 package ru.netology
 
 fun main() {
-    val previousPurchaseAmount = 3_000
-    val purchaseAmount = 5_000
-    val discountStart = 10_000
-    val discountMedium = 1_000_000
+    val previousPurchaseAmountKopeck = 3_000
+    val purchaseAmountKopeck = 1_200_000
+    val limitDiscountStartKopeck = 100_000 // коп.
+    val limitDiscountMediumKopeck = 1_000_000 // коп.
+    val discountStartKopeck = 10_000 // 100 руб.
+    val discountMediumPercent = 5 // 5%
+    val extraDiscountPercent = 1 // 1
+    val customerRegular = true
+    println("Покупка: ${purchaseAmountKopeck / 100} руб. ${purchaseAmountKopeck % 100} " +
+            "${previousPurchaseAmountKopeck % 10} коп.")
 
-    val priceDiscountStart = purchaseAmount * discountStart
-    val priceDiscountMedium = purchaseAmount * discountMedium
-    if ( discountMedium > priceDiscountStart >  || < priceDiscountMedium)
+    val priceDiscountStartKopeck = purchaseAmountKopeck - discountStartKopeck
+    val priceDiscountMedium: Int = purchaseAmountKopeck - (purchaseAmountKopeck * discountMediumPercent / 100)
+    val preResultKopeck = if (previousPurchaseAmountKopeck < limitDiscountStartKopeck) purchaseAmountKopeck else if
+            ((previousPurchaseAmountKopeck > limitDiscountStartKopeck) &&
+                (previousPurchaseAmountKopeck < limitDiscountMediumKopeck))
+                    priceDiscountStartKopeck else priceDiscountMedium
+    println("Покупка после применения скидки: ${preResultKopeck / 100} руб. ${preResultKopeck % 100} " +
+            "${preResultKopeck % 10} коп.")
+
+    val priceExtraDiscount: Int = preResultKopeck - (preResultKopeck * extraDiscountPercent / 100)
+    val result = if (customerRegular) priceExtraDiscount else preResultKopeck
+    println("Покупка после применения скидки постоянного покупателя: ${result / 100} руб. ${result % 100} " +
+            "${result % 10} коп. ")
+
 }
